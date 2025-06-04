159cf2c00e4a79ef08d36cd043115516ec43eed0
liubin <bh0bama@sina.com>
2025-06-04 15:01:59 +0800

    override styles

--- src/components/BubbleMenuItems.tsx
+++ src/components/BubbleMenuItems.tsx
@@ -1,31 +1,63 @@
 import { useCurrentEditor } from "@tiptap/react";
-import React from "react";
-import "./bubble.menu.items.scss";
+import "./bubble-menu-items.scss";
+import { MarkButton } from "./tiptap-ui/mark-button";
+import { HeadingButton } from "./tiptap-ui/heading-button";
 
 export default function BubbleMenuItems() {
   const { editor } = useCurrentEditor();
-  if (!editor) return <div className="bubble-menu">loading...</div>;
+  if (!editor) return <div className="hw-bubble-menu">loading...</div>;
 
   return (
-    <div className="bubble-menu">
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        className={editor.isActive("bold") ? "is-active" : ""}
-      >
-        Bold
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        className={editor.isActive("italic") ? "is-active" : ""}
-      >
-        Italic
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        className={editor.isActive("strike") ? "is-active" : ""}
-      >
-        Strike
-      </button>
+    <div
+      className="hw-bubble-menu tiptap-button-group"
+      data-orientation="horizontal"
+    >
+      <HeadingButton level={1}></HeadingButton>
+      <HeadingButton level={2}></HeadingButton>
+      <HeadingButton level={3}></HeadingButton>
+      <HeadingButton level={4}></HeadingButton>
+      <MarkButton
+        type="bold"
+        className={`hw-editor-button ${
+          editor.isActive("bold") ? "is-active" : ""
+        }`}
+      />
+      <MarkButton
+        type="italic"
+        className={`hw-editor-button ${
+          editor.isActive("italic") ? "is-active" : ""
+        }`}
+      />
+      <MarkButton
+        type="strike"
+        className={`hw-editor-button ${
+          editor.isActive("strike") ? "is-active" : ""
+        }`}
+      />
+      <MarkButton
+        type="code"
+        className={`hw-editor-button ${
+          editor.isActive("code") ? "is-active" : ""
+        }`}
+      />
+      <MarkButton
+        type="underline"
+        className={`hw-editor-button ${
+          editor.isActive("underline") ? "is-active" : ""
+        }`}
+      />
+      <MarkButton
+        type="superscript"
+        className={`hw-editor-button ${
+          editor.isActive("superscript") ? "is-active" : ""
+        }`}
+      />
+      <MarkButton
+        type="subscript"
+        className={`hw-editor-button ${
+          editor.isActive("subscript") ? "is-active" : ""
+        }`}
+      />
     </div>
   );
 }
--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -1,20 +1,45 @@
 // src/Tiptap.tsx
-import React from "react";
-import { EditorProvider, FloatingMenu, BubbleMenu } from "@tiptap/react";
+import { EditorProvider, BubbleMenu, AnyExtension } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import BubbleMenuItems from "./BubbleMenuItems";
+import { MarkButton } from "@/components/tiptap-ui/mark-button";
+import "@/styles/_variables.scss";
+import "@/styles/_keyframe-animations.scss";
+import "@/components/tiptap-node/code-block-node/code-block-node.scss";
+import "@/components/tiptap-node/paragraph-node/paragraph-node.scss";
 
 // define your extension array
-const extensions = [StarterKit];
+const extensions = [StarterKit, MarkButton] as AnyExtension[];
 
-const content =
-  "<p>ions allow you to add attributes to the rendered HTML through the HTMLAttributes option. You can use that to add a custom class (or any other attribute). That's also very helpfulions allow you to add attributes to the rendered HTML through the HTMLAttributes option. You can use that to add a custom class (or any other attribute). That's also very helpfulions allow you to add attributes to the rendered HTML through the HTMLAttributes option. You can use that to add a custom class (or any other attribute). That's also very helpfulions allow you to add attributes to the rendered HTML through the HTMLAttributes option. You can use that to add a custom class (or any other attribute). That's also very helpfulions allow you to add attributes to the rendered HTML through the HTMLAttributes option. You can use that to add a custom class (or any other attribute). That's also very helpfulions allow you to add attributes to the rendered HTML through the HTMLAttributes option. You can use that to add a custom class (or any other attribute). That's also very helpfulƒ!</p>";
-
+const content = `
+        <p>
+            <strong>Bold</strong> for emphasis with <code>**</code> or <code>⌘+B</code> or the <code>B</code> button.
+        </p>
+        <p>
+            <em>Italic</em> for subtle nuances with <code>*</code> or <code>⌘+I</code> or the <code>I</code> button.
+        </p>
+        <p>
+            <s>Strikethrough</s> to show revisions with <code>~~</code> or the <code>~~S~~</code> button.
+        </p>
+        <p>
+            <code>Code</code> for code snippets with <code>:</code> or <code>⌘+⇧+C</code> or the <code>C</code> button.
+        </p>
+        <p>
+            <u>Underline</u> for emphasis with <code>⌘+U</code> or the <code>U</code> button.
+        </p>
+        <p>
+            <sup>Superscript</sup> for footnotes with <code>⌘+.</code> or the <code>.</code> button.
+        </p>
+        <p>
+            <sub>Subscript</sub> for chemical formulas with <code>⌘+,</code> or the <code>,</code> button.
+        </p>
+        `;
 const Tiptap = () => {
   return (
     <EditorProvider
       extensions={extensions}
       content={content}
+      immediatelyRender={false}
       slotBefore={
         <div style={{ background: "black", color: "white" }}>slotBefore</div>
       }
@@ -23,9 +48,6 @@ const Tiptap = () => {
       }
       editorContainerProps={{ className: "editor-container" }}
     >
-      {/* <FloatingMenu editor={null}>
-        <h1>Hello Floating....</h1>
-      </FloatingMenu> */}
       <BubbleMenu editor={null} tippyOptions={{ duration: 100 }}>
         <BubbleMenuItems />
       </BubbleMenu>
